303ad8c6946c354fba8144b6dbdc68d543bbdbe4
FISH BIG <1025092148@qq.com>
2022-03-21 12:51:04 +0100

    Added 5 new methods in LegoSetRepository Class and creat MainClass

--- src/main/java/brickset/LegoSetRepository.java
+++ src/main/java/brickset/LegoSetRepository.java
@@ -1,6 +1,9 @@
 package brickset;
-
 import repository.Repository;
+import java.util.Comparator;
+import java.util.Objects;
+import java.util.OptionalDouble;
+import java.util.stream.Stream;
 
 /**
  * Represents a repository of {@code LegoSet} objects.
@@ -11,4 +14,37 @@ public class LegoSetRepository extends Repository<LegoSet> {
         super(LegoSet.class, "brickset.json");
     }
 
+    public Stream<LegoSet> printLegoSetWithHighestNumber(){//Method_1
+       return getAll().stream().
+                sorted(Comparator.comparing(LegoSet::getNumber)).
+                filter(LegoSet->LegoSet.getName()!=null).
+                limit(1)
+               ;
+    }
+    public Stream<LegoSet> printLegoSetWithFullInformation(){//Method_2
+         return getAll().stream().
+                 filter(Objects::nonNull);
+    }
+    public Stream<LegoSet> printLegoSetWith(int pieces){
+        return getAll().stream().
+                filter(legoSet -> legoSet.getPieces()==pieces&&legoSet.getName()!=null);
+    }
+    public Stream<Boolean> printLegoSetNameStartWith(String pattern){
+        return getAll().stream().
+                map(LegoSet::getName).
+                map(String::toLowerCase).
+                map(s->s.startsWith(pattern))
+                ;
+    }
+    public Double printLegoSetWithAveragePieces(){
+        return getAll().stream().
+                mapToInt(LegoSet::getPieces).
+                filter(Objects::nonNull).
+                average().
+                getAsDouble()
+                ;
+
+    }
+
 }
+
---
+++ src/main/java/brickset/Main.java
@@ -0,0 +1,13 @@
+package brickset;
+
+public class Main {
+    public static void main(String[] args) {
+        var repository = new LegoSetRepository();
+        System.out.println(repository.printLegoSetWithHighestNumber());
+        System.out.println(repository.printLegoSetWithFullInformation());
+        System.out.println(repository.printLegoSetWith(12));
+        System.out.println(repository.printLegoSetNameStartWith("a"));
+        System.out.println(repository.printLegoSetWithAveragePieces());
+
+    }
+}
